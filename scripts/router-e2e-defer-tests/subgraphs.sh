5d149b4fa0a8aa3f28f83f73a926078897ba8dca
alessia <alessia@apollographql.com>
2022-09-15 12:17:49 -0400

    chore(tests): remove pandas subgraph

--- scripts/router-e2e-defer-tests/subgraphs.sh
+++ scripts/router-e2e-defer-tests/subgraphs.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-subgraphs=("products" "users" "pandas")
+subgraphs=("products" "users")
 
 url_products="http://products:4000/graphql"
 url_users="http://users:4000/graphql"
-url_pandas="http://pandas:4000/graphql"
 
 schema_products="subgraphs/products/products.graphql"
 schema_users="subgraphs/users/users.graphql"
-schema_pandas="subgraphs/pandas/pandas.graphql"
