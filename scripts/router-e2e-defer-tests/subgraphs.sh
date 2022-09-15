7de81ebd2c17e706b5db17a8c0e17b8c666c30ca
alessia <alessia@apollographql.com>
2022-09-15 12:12:42 -0400

    chore(tests): remove reviews subgraph

--- scripts/router-e2e-defer-tests/subgraphs.sh
+++ scripts/router-e2e-defer-tests/subgraphs.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-subgraphs=("products" "users" "pandas" "reviews")
+subgraphs=("products" "users" "pandas")
 
 url_products="http://products:4000/graphql"
 url_users="http://users:4000/graphql"
 url_pandas="http://pandas:4000/graphql"
-url_reviews="http://reviews:4000/graphql"
 
 schema_products="subgraphs/products/products.graphql"
 schema_users="subgraphs/users/users.graphql"
 schema_pandas="subgraphs/pandas/pandas.graphql"
-schema_reviews="subgraphs/reviews/reviews.graphql"
