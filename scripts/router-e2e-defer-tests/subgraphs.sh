396f63d6f98a6764d8617bafbdbd4297452955b6
alessia <alessia@apollographql.com>
2022-09-15 12:24:41 -0400

    chore(tests): remove users subgraph

--- scripts/router-e2e-defer-tests/subgraphs.sh
+++ scripts/router-e2e-defer-tests/subgraphs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-subgraphs=("products" "users")
+subgraphs=("products")
 
 url_products="http://products:4000/graphql"
-url_users="http://users:4000/graphql"
 
 schema_products="subgraphs/products/products.graphql"
-schema_users="subgraphs/users/users.graphql"
