1ddcb3859f962d5f6816733eabcdb27a2b59c051
Alessia Bellisario <alessia@apollographql.com>
2023-04-12 17:50:28 -0400

    Reinstate test for non-nullable errors thrown outside deferred fragments (#98)
    * un-skip test for non-nullable errors thrown outside deferred fragments
    
    * remove comment
    
    * remove loading assertion
    
    * chore: test against snapshot release of fix
    
    * Update package.json
    
    * update lockfile

--- CRA-demo/cypress/e2e/defer.cy.js
+++ CRA-demo/cypress/e2e/defer.cy.js
@@ -105,13 +105,13 @@ describe("@defer tests", () => {
     cy.findAllByText(/apollo-client/i).should("exist");
   });
 
-  // Skipping until AC bug https://github.com/apollographql/apollo-client/issues/10406 is resolved
   // https://github.com/graphql/graphql-js/blob/a24a9f35b876bdd0d5050eca34d3020bd0db9a29/src/execution/__tests__/defer-test.ts#L497
-  it.skip("Handles non-nullable errors thrown outside deferred fragments", () => {
+  it("Handles non-nullable errors thrown outside deferred fragments", () => {
     cy.visit("/error-non-nullable-outside-deferred-fragment");
-    cy.findByText(/loading/i).should("exist");
     cy.findByText(/Error :\(/i).should("exist");
     cy.findAllByText(/apollo-federation/i).should("not.exist");
+    cy.findAllByText(/apollo-studio/i).should("not.exist");
+    cy.findAllByText(/apollo-client/i).should("not.exist");
   });
 
   // https://github.com/graphql/graphql-js/blob/a24a9f35b876bdd0d5050eca34d3020bd0db9a29/src/execution/__tests__/defer-test.ts#L167
